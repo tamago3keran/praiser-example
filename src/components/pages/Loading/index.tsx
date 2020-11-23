d7a21992743662e42bd6fbcd4a4b3334eaf15764
tamago3keran <yamataku3831@gmail.com>
2020-11-23 11:07:06 +0900

    Create Stack Navigation

--- src/components/pages/Loading/index.tsx
+++ src/components/pages/Loading/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as UiContext from '../../../contexts/ui';
 
 const styles = StyleSheet.create({
   container: {
@@ -9,10 +10,22 @@ const styles = StyleSheet.create({
   },
 });
 
+function ChangeStateButton(props: { state: UiContext.Status }) {
+  const { setApplicationState } = React.useContext(UiContext.Context);
+  const { state } = props;
+  return (
+    <TouchableOpacity onPress={() => setApplicationState(state)}>
+      <Text>Change state to {state}</Text>
+    </TouchableOpacity>
+  );
+}
+
 export default function Loading() {
   return (
     <View style={styles.container}>
-      <Text>Loading</Text>
+      <ChangeStateButton state={UiContext.Status.AUTHORIZED} />
+      <ChangeStateButton state={UiContext.Status.UN_AUTHORIZED} />
+      <ChangeStateButton state={UiContext.Status.FIRST_OPEN} />
     </View>
   );
 }
