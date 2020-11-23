736fa394986c18d00dc255f19edfd0d767d1c9b3
tamago3keran <yamataku3831@gmail.com>
2020-11-23 11:24:57 +0900

    Create Tab Navigation

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -2,3 +2,4 @@ export { default as ChooseLogin } from './ChooseLogin';
 export { default as Home } from './Home';
 export { default as Initial } from './Initial';
 export { default as Loading } from './Loading';
+export { default as Statistics } from './Statistics';
--- src/routes/Main/index.tsx
+++ src/routes/Main/index.tsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import { createStackNavigator, StackCardInterpolationProps } from '@react-navigation/stack';
-import { ChooseLogin, Home, Initial, Loading } from '../../components/pages';
-import { CHOOSE_LOGIN, HOME, INITIAL, LOADING } from '../../constants/path';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { ChooseLogin, Home, Initial, Loading, Statistics } from '../../components/pages';
+import { CHOOSE_LOGIN, HOME, INITIAL, LOADING, STATISTICS } from '../../constants/path';
 import * as UiContext from '../../contexts/ui';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 const forFade = ({ current }: StackCardInterpolationProps) => ({
   cardStyle: {
     opacity: current.progress,
   },
 });
 
+function TabRoutes() {
+  return (
+    <Tab.Navigator initialRouteName={HOME}>
+      <Tab.Screen name={HOME} component={Home} />
+      <Tab.Screen name={STATISTICS} component={Statistics} />
+    </Tab.Navigator>
+  );
+}
+
 function switchingAuthStatus(status: UiContext.Status) {
   switch (status) {
     case UiContext.Status.UN_AUTHORIZED:
       return <Stack.Screen name={CHOOSE_LOGIN} component={ChooseLogin} />;
     case UiContext.Status.AUTHORIZED:
-      return <Stack.Screen name={HOME} component={Home} />;
+      return <Stack.Screen name={HOME} component={TabRoutes} />;
     case UiContext.Status.FIRST_OPEN:
     default:
       return <Stack.Screen name={INITIAL} component={Initial} />;
